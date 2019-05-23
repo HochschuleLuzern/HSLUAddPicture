2dd975284f4263f9014ad9a1ebd320e51aaf7781
Stephan Winiker <stephan.winiker@hslu.ch>
2019-05-23 12:36:35 +0200

    Update for ILIAS 5.4

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = 'hsluaddpic';
 $version = '0.1';
 $ilias_min_version = '5.3';
-$ilias_max_version = '5.3.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'Stephan Winiker';
 $responsible_mail = 'stephan.winiker@hslu.ch';
 ?>
