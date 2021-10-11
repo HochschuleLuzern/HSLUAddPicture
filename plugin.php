615a4ceec62ece730e9ae11c148ec6f2035f554e
Stephan Winiker <stephan.winiker@hslu.ch>
2021-10-11 18:30:01 +0200

    Update for ILIAS7

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'hsluaddpic';
 $version = '0.1';
-$ilias_min_version = '6.0';
-$ilias_max_version = '6.999';
+$ilias_min_version = '7.0';
+$ilias_max_version = '7.999';
 $responsible = 'Stephan Winiker';
 $responsible_mail = 'stephan.winiker@hslu.ch';
 ?>
